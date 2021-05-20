72c7f0a60d133519f7884d6edbc66b37f9f3efc2
Nguyen Duc Huy <nxhuy@zonemail.clpccd.edu>
2021-05-20 19:15:01 +0700

    better code

--- Two Knights.cpp
+++ Two Knights.cpp
@@ -84,27 +84,31 @@ void __print_collection(T const &container)
     for (auto pos : container)
         cout << pos << " ";
 }
+// int main()
+// {
+//     long long int n;
+//     cin >> n;
+//     vector<long long int> a = {0};
+//     cout << a[0] << endl;
+//     long long int c = 2;
+//     long long int temp = a[0];
+
+//     while (c < n)
+//     {
+//  c++;
+//         temp = temp + (((c - 1) * (c - 1) - 2) * 2 + ((c - 1) * (c - 1) - 3) * 2 + ((c - 1) * (c - 1) - 4) * (c - 1 - 4)) * 2 + ((c - 1) * (c - 1) - 2) + (2 * c - 1) * (c - 1) - 2;
+//         cout << temp << endl;
+//         c++;
+//     }
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
 int main()
 {
-    // read;
-    long long int m;
-    cin >> m;
-    vt<int> a = {0, 6, 28, 96};
-    if (m <= 4)
-        FOR(i, 0, m)
-    cout << a[i] << endl;
-    else
-    {
-        FOR(i, 0, 4)
-        cout << a[i] << endl;
-        long long int n = 5;
-        long long int temp = a[3];
+    long long int n;
+    cin >> n;
 
-        while (n <= m)
-        {
-            temp = temp + (((n - 1) * (n - 1) - 2) * 2 + ((n - 1) * (n - 1) - 3) * 2 + ((n - 1) * (n - 1) - 4) * (n - 1 - 4)) * 2 + ((n - 1) * (n - 1) - 2) + (2 * n - 1) * (n - 1) - 2;
-            cout << temp << endl;
-            n++;
-        }
-    }
+    for (long long int i = 1; i <= n; i++)
+        cout << (i * i) * (i * i - 1) / 2 - 4 * ((i - 1) * (i - 2)) << endl;
 }
